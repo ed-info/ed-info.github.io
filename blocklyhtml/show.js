b0b58b7648c2dec51a33174048b8cc20622e8a3a
ed-info <116575319+ed-info@users.noreply.github.com>
2023-05-21 11:26:36 +0300

    Add files via upload

---
+++ blocklyhtml/show.js
@@ -0,0 +1,8 @@
+function showMePreview(){
+  document.getElementById("websiteFrame").style.display = 'block';
+  document.getElementById("sourcecode").style.display = 'none';
+}
+function showMeCode(){
+  document.getElementById("websiteFrame").style.display = 'none';
+  document.getElementById("sourcecode").style.display = 'block';
+}
