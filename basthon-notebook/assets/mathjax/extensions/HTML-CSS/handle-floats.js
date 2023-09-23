f489bd5979183fcf944b0d437b7c0265c525aef2
ed-info <116575319+ed-info@users.noreply.github.com>
2023-09-23 12:45:59 +0300

    Add files via upload

---
+++ basthon-notebook/assets/mathjax/extensions/HTML-CSS/handle-floats.js
@@ -0,0 +1,1 @@
+MathJax.Extension["HTML-CSS/handle-floats"]={version:"2.7.9"},MathJax.Hub.Startup.signal.Post("HTML-CSS handle-floats Ready"),MathJax.Ajax.loadComplete("[MathJax]/extensions/HTML-CSS/handle-floats.js");
