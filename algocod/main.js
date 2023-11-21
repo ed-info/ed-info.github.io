d2d91a0519ae006fccb656cf921019b98a92980f
Gromko Ed-Info <computer.science.ready@gmail.com>
2023-11-21 19:42:19 +0200

    Added some function for AlgoCod

--- algocod/main.js
+++ algocod/main.js
@@ -2,10 +2,36 @@ const codeEditor = CodeMirror(document.querySelector(".editor"), {
     lineNumbers: true,
     tabSize: 4,
     theme: "pastel-on-dark",
-    mode: "pseudocode"
+    mode: "pseudocode",
+    extraKeys: {
+            "Tab": function(cm){
+              cm.replaceSelection("   " , "end");
+            }
+           }
 })
-
+// Клавіатурні скорочення для вставлення мовних конструкцій
+var mymap = {
+	"Alt-Z": function(cm){codeEditor.doc.replaceSelection('якщо   то\n\n'+" ".repeat(codeEditor.getCursor().ch)+'кін якщо\n');},
+	"Alt-S": function(cm){codeEditor.doc.replaceSelection('якщо   то\n\n'+" ".repeat(codeEditor.getCursor().ch)+'інакше\n\n'+" ".repeat(codeEditor.getCursor().ch)+'кін якщо\n');},
+	"Alt-L": function(cm){codeEditor.doc.replaceSelection('для   від  до\n\n'+" ".repeat(codeEditor.getCursor().ch)+'кін для\n');},
+	"Alt-G": function(cm){codeEditor.doc.replaceSelection('поки\n\n'+" ".repeat(codeEditor.getCursor().ch)+'кін поки\n');},
+	"Alt-A": function(cm){codeEditor.doc.replaceSelection('функція\n\n'+" ".repeat(codeEditor.getCursor().ch)+'кін функція\n');},
+	"Alt-F": function(cm){codeEditor.doc.replaceSelection('анімація\n\n'+" ".repeat(codeEditor.getCursor().ch)+'кін анімація\n');},
+	"Alt-1": function(cm){codeEditor.doc.replaceSelection('лінія(');},
+	"Alt-2": function(cm){codeEditor.doc.replaceSelection('прямокутник(');},
+	"Alt-3": function(cm){codeEditor.doc.replaceSelection('трикутник(');},
+	"Alt-4": function(cm){codeEditor.doc.replaceSelection('чотирикутник(');},
+	"Alt-5": function(cm){codeEditor.doc.replaceSelection('заповнення(');},
+	"Alt-6": function(cm){codeEditor.doc.replaceSelection('контур');},
+	"Alt-7": function(cm){codeEditor.doc.replaceSelection('ламана\n'+" ".repeat(codeEditor.getCursor().ch)+'\tвершина(,)\n'+" ".repeat(codeEditor.getCursor().ch)+'\tвершина(,)\n'+" ".repeat(codeEditor.getCursor().ch)+'кін ламана\n');},	
+	"Alt-8": function(cm){codeEditor.doc.replaceSelection('фігура\n'+" ".repeat(codeEditor.getCursor().ch)+'\tвершина(,)\n'+" ".repeat(codeEditor.getCursor().ch)+'\tвершина(,)\n'+" ".repeat(codeEditor.getCursor().ch)+'кін фігура\n');},	
+	"Alt-0": function(cm){codeEditor.doc.replaceSelection('коло(');},
+	"Alt-I": function(cm){console.log(codeEditor.getCursor().ch);}
+	}
+codeEditor.addKeyMap(mymap);
+codeEditor.focus();
 codeEditor.setSize("100%", "100%")
+
 
 function output() {
     let element = document.createElement("p")
@@ -39,8 +65,9 @@ function greenOutput() {
     element.setAttribute("class", "outputLine")
     document.querySelector(".runner").appendChild(element)
 }
-
+var onError;
 function error(errorCode) {
+	onError = true;
     let element = document.createElement("p")
     element.innerHTML = errorCode
     element.innerHTML = "<span style='color: red;'>"+element.innerHTML
@@ -70,26 +97,43 @@ window.onerror = function(e) {
   };
 
 
-
+function endOfRun(){
+	greenOutput("······ виконання програми закінчено ······");
+	codeEditor.focus()
+}
 
 
 function runCode() {
+		
     document.querySelector(".runner").innerHTML = ""
     let compiledCode = compile(codeEditor.getValue())
 
     if (compiledCode === false) {
         return
     }
-greenOutput("······ виконання програми розпочато ······");
-	console.log(compiledCode)
-
+	greenOutput("······ виконання програми розпочато ······");
+	onError = false;
+	animateOn = false;
     var codeToRun = new Function(compiledCode)
 
 		codeToRun()
-greenOutput("······ виконання програми закінчено ······");
+	
 }
 
+function run(e){
+  if (e.innerText==="▶"){
+    e.innerText="■";
+    runCode();
+    if (animateOn===false){endOfRun();e.innerText="▶"}
+   }
+  else {e.innerText="▶";
+	  onError = true }
+ }
+ 
+ 
+   
+/*
 document.querySelector("#run").addEventListener("click", function () {
     runCode()
 }, {passive: true})
-
+*/
