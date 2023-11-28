5b203f7d31883bd8616411e3eafae3aea7567847
Gromko Ed-Info <computer.science.ready@gmail.com>
2023-11-28 16:26:56 +0200

    js map and utf-8 add

--- algocod/mode/syntaxhighlight.js
+++ algocod/mode/syntaxhighlight.js
@@ -5,7 +5,7 @@ CodeMirror.defineSimpleMode("pseudocode", {
       // groups must span the whole string in this case
       {regex: /(function)(\s+)([a-z$][\w$]*)/, token: ["keyword", null, "variable-2"]},
 	  {regex: /(?:method|return|if|while|else|loop|until|from|then|output|input|end|to|Math)\b/, token: "keyword"},
-      {regex: /функція|вершина|ламана|фігура|контур|товщина|анімація|результат|якщо|поки|інакше|цикл|для|не|від|то|вивести|ввести|кін|безанімації|до|точка|лінія|прямокутник|трикутник|коло|еліпс|фон|колір|чотирикутник|заповнення/, token: "keyword"},
+      {regex: /функція|вершина|дуга|ламана|фігура|контур|товщина|анімація|результат|якщо|поки|інакше|цикл|для|не|від|то|вивести|ввести|кін|безанімації|до|точка|лінія|прямокутник|трикутник|коло|еліпс|фон|колір|чотирикутник|заповнення/, token: "keyword"},
       {regex: /true|false|істина|хиба|null|undefined/, token: "atom"},
       {regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i, token: "number"},
       {regex: /\/\/.*/, token: "comment"},
